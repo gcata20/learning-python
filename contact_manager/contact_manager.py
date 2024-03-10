158e3e96c4d39892287784cbfe6c85551c775255
Catalin Gherhes <gcatalintm@gmail.com>
2024-03-10 20:01:15 +0200

    Add more Contact attributes. Write a function that returns a string for printing Contact objects.

--- contact_manager/contact_manager.py
+++ contact_manager/contact_manager.py
@@ -1,17 +1,19 @@
+from tabulate import tabulate
+import json
 import re
 
 
 class Contact:
-    def __init__(self, name: str, email: str) -> None:
+    def __init__(self, name, email="", phone="", birthday="", note="") -> None:
         self.name = name
         self.email = email
-
+        self.phone = phone
+        self.birthday = birthday
+        self.note = note
+    
     def __str__(self) -> str:
-        return f"""
-            Contact object/instance.
-            Name: {self._name}
-            Email: {self._email}
-        """
+        s = tabulate([self.__dict__], headers="keys", tablefmt="simple_grid")
+        return s
     
     @property
     def name(self) -> str:
@@ -29,7 +31,7 @@ class Contact:
     
     @email.setter
     def email(self, email) -> None:
-        pattern = r'[^@]+@[^@]+'
+        pattern = r'[0-9a-zA-Z_\-\.]+@[^@]+'
         if not re.fullmatch(pattern, email):
             raise ValueError('Creation failed. Email not valid.')
         self._email = email
@@ -40,15 +42,26 @@ class Contact:
 
         name = input('Name: ').strip()
         email = input('Email: ').strip()
+        phone = input('Phone: ').strip()
+        birthday = input('Birthday (YYYY-MM-DD): ').strip()
+        note = input('Note: ').strip()
         try:
-            return cls(name, email)
+            return cls(name, email, phone, birthday, note)
         except ValueError as err:
             print(err)
 
 
 def main():
-    new_contact = Contact.get()
-    print(new_contact)
+    # input_contacts = [
+    #     Contact('contact_1', 'name-1@fake-domain'),
+    #     Contact('contact_2', 'name-2@fake-domain', '0565146935'),
+    #     Contact('contact_3', 'name-3@fake-domain', '', '05-11-1956'),
+    #     Contact('contact_4', 'name-4@fake-domain', '', '', 'This is a note.')
+    # ]
+    # contacts_to_write = [con.__dict__ for con in input_contacts]
+    # with open('contacts.json', 'w') as f:
+    #     json.dump(contacts_to_write, f, indent=4)
+    ...
 
 
 if __name__ == '__main__':
