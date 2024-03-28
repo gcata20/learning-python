b540b524f79f3993ffb3b867ad0d40bb0cc58d91
Catalin Gherhes <gcatalintm@gmail.com>
2024-03-28 17:11:27 +0200

    Initial commit.
    Preparing to implement Pazaak, a card game from the Star Wars franchise.

---
+++ pazaak/pazaak.py
@@ -0,0 +1,6 @@
+"""
+Pazaak, a game dating back to Old Republic times, was a popular card game
+in which the goal was to come closest to 20 without going over. The player
+with the highest score less than or equal to 20 won the round, and the player
+who won three rounds won the match.
+"""
