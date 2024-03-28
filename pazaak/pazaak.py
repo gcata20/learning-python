6645fcca81798e1560ae8d21cd4e07c8d2e7da83
Catalin Gherhes <gcatalintm@gmail.com>
2024-03-28 23:21:53 +0200

    Implement initialization logic.
    Add function for generating the house deck.
    Add function for generating player hands from side decks.

--- pazaak/pazaak.py
+++ pazaak/pazaak.py
@@ -1,6 +1,40 @@
-"""
-Pazaak, a game dating back to Old Republic times, was a popular card game
-in which the goal was to come closest to 20 without going over. The player
-with the highest score less than or equal to 20 won the round, and the player
-who won three rounds won the match.
-"""
+from random import choice, randint, shuffle
+
+
+def main():
+    house_deck = get_house_deck()
+    player_hand = get_hand()
+    computer_hand = get_hand()
+
+
+def get_hand() -> list[str]:
+    """Return a list representing a player's 4-card hand."""
+    deck = []
+    while len(deck) < 10:
+        value = str(randint(1, 6))
+        mod = choice(['+', '-', '±'])
+        card = mod + value
+        if deck.count(card) == 2:
+            continue
+        deck.append(card)
+
+    hand = []
+    for _ in range(4):
+        shuffle(deck)
+        card = deck.pop()
+        hand.append(card)
+    return hand
+
+
+def get_house_deck() -> list[str]:
+    """Return a list representing the 40-card house deck."""
+
+    deck = []
+    for i in range(10):
+        for _ in range(4):
+            deck.append(str(i + 1))
+    return deck
+
+
+if __name__ == '__main__':
+    main()
